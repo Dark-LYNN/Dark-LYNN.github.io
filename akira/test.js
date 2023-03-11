daba60d7bc198201165ca419f250cbefad6d1a8f
Dark-LYNN <dieenebelggaetan@gmail.com>
2023-03-11 17:19:22 +0100

    Update test.js

--- akira/test.js
+++ akira/test.js
@@ -1,121 +1,187 @@
 module.exports = ({
-    name: "giverole",
-    description: "Quickly assign a role to a member.",
+    name: "commandinfo",
+    aliases: ['command-info','command'],
     $if: "old",
-    usage: "$getServerVar[prefix]giverole <user> <role> {reason}",
+    description: "show info about akira",
+    usage: "$getServerVar[prefix]commandinfo",
     code: `
-    $if[$getServerVar[moderation]==enabled]
-        $if[$getServerVar[giverole]==enabled]
-            $if[$getChannelVar[ignored]==false]
-                $if[$checkContains[$userPerms[$authorID];manageroles;admin]==true]
-                    $if[$message!=]
-                        $if[$findRole[$message[2]]!=]
-                            $if[$rolePosition[$findRole[$message[2]];$guildID]>$rolePosition[$highestRole[$clientID;$guildID;id];$guildID]]
-                                $if[$message[3]!=]
-                                    $color[$getVar[color]]
-                                    $giveRole[$guildID;$findUser[$message[1]];$findRole[$message[2]]]
-                                    $if[$getServerVar[botLogChannel]==1083095711094149180]
-                                    $else
-                                        $channelSendMessage[$getServerVar[botLogChannel];{newEmbed:{color:$getServerVar[color]}{title:Role given}{thumbnail:$if[$serverIcon==] https://cdn.lynnux.xyz/images/No-Server_Icon-found.png $else $serverIcon $endif}{footer:Given by @$userTag}{description:The <@&$findRole[$message[2]]> has been given to <@$findUser[$message[1]]>.
-Reason: \`\`\`
-$replaceText[$replaceText[$message;$message[2] ;;1];$message[1] ;;1]
-\`\`\`}}]
-                                    $endif
-                                    $title[Role given]
-                                    $description[The <@&$findRole[$message[2]]> has been given to <@$findUser[$message[1]]>.
-Reason: \`\`\`
-$replaceText[$replaceText[$message;$message[2] ;;1];$message[1] ;;1]
-\`\`\`]
-                                    $if[$serverIcon==]
-                                        $thumbnail[https://cdn.lynnux.xyz/images/No-Server_Icon-found.png]
-                                    $else
-                                        $thumbnail[$serverIcon]
-                                    $endif
+        $title[Commandinfo: "$message[1]"]
+        $color[$getServerVar[color]]
+        $if[$checkContains[$toLowerCase[$message[1]];about;ping]==true]
+        $description[**Command:** \`about\`
+    **aliases:** [\`ping\`\] 
+    **description:**  \`Check the details about akira like latancy etc\`
+    **Usage:** \`akira about\`
+    
+    **Example:**
+    \`\`\`
+    akira about
+    \`\`\`
+    **Module\:** \`Utility\`
+    ]
+        $else
+            $if[$checkContains[$toLowerCase[$message[1]];whois;who-is;user;userinfo;user-info]==true]
+            $description[**Command:** \`userinfo\`
+    **aliases:** [\`user-info\`,\`user\`,\`whois\`,\`who-is\`\] 
+    **description:**  \`Check the details about a user\`
+    **Usage:** \`akira userinfo [user]\`
+    
+    **Example:**
+    \`\`\`
+    akira userinfo @Dark-LYNN#6568
+    akira userinfo 705306248538488947
+    \`\`\`
+    **Module\:** \`Utility\`
+    ]		
+            $else
+                $if[$checkContains[$toLowerCase[$message[1]];commandinfo;command-info;command]==true]
+                    $description[**Command:** \`commandinfo\`
+    **aliases:** [\`command-info\`,\`command\`\] 
+    **description:**  \`Check the details about a user\`
+    **Usage:** \`akira commandinfo [command]\`
+    
+    **Example:**
+    \`\`\`
+    akira commandinfo ping
+    \`\`\`
+    **Module\:** \`Help\`
+    ]		
+                $else
+                    $if[$checkContains[$toLowerCase[$message[1]];balance;bal;money;wallet]==true]
+                    $description[**Command:** \`balance\`
+    **aliases:** [\`bal\`,\`money\`,\`wallet\`\] 
+    **description:**  \`Check how much money is in your wallet and in your bank.\`
+    **Usage:** \`akira balance <user>\`
+    
+    **Example:**
+    \`\`\`
+    akira balance
+    akira balance @Dark-LYNN#6568
+    \`\`\`
+    **Module\:** \`Economy\`
+    ]		
+                    $else
+                        $if[$checkContains[$toLowerCase[$message[1]];deposit;dep]==true]
+                        $description[**Command:** \`deposit\`
+    **aliases:** [\`dep\`\] 
+    **description:**  \`Deposit money from your wallet to your bank.\`
+    **Usage:** \`akira deposit [amount]\`
+    
+    **Example:**
+    \`\`\`
+    akira deposit 100
+    \`\`\`
+    **Module\:** \`Economy\`
+    ]
+                        $else
+                            $if[$checkContains[$toLowerCase[$message[1]];withdraw;with]==true]
+                            $description[**Command:** \`withdraw\`
+    **aliases:** [\`with\`\] 
+    **description:**  \`Withdraw money from your bank to your wallet.\`
+    **Usage:** \`akira withdraw [amount]\`
+    
+    **Example:**
+    \`\`\`
+    akira withdraw 100
+    \`\`\`
+    **Module\:** \`Economy\`
+    ]
+                            $else
+                                $if[$checkContains[$toLowerCase[$message[1]];eval;ev;djs]==true]
+                                $description[**Command:** \`eval\`
+    **aliases:** [\`ev\`,\`djs\`\] 
+    **description:**  \`Run a d.js/javascript code.\`
+    **Usage:** \`akira eval [code]\`
+    
+    **Example:**
+    \`\`\`
+    akira eval message.channel.send('hello world')
+    \`\`\`
+    **Module\:** \`Dev\`
+    ]
                                 $else
-                                    $giveRole[$guildID;$findUser[$message[1]];$findRole[$message[2]]]
-                                    $color[$getVar[color]]
-                                    $if[$getServerVar[botLogChannel]==1083095711094149180]
+                                    $if[$checkContains[$toLowerCase[$message[1]];serverinfo;server-info;server;guildinfo;guild-info;guild]==true]
+                                        $description[**Command:** \`ServerInfo\`
+    **aliases:** [\`server-info\`,\`server\`,\`guildinfo\`,\`guild-info\`,\`guild\`\] 
+    **description:**  \`show info about the current server.\`
+    **Usage:** \`akira serverinfo\`
+    
+    **Example:**
+    \`\`\`
+    akira serverinfo
+    \`\`\`
+    **Module\:** \`Utility\`
+    ]
                                     $else
-                                        $channelSendMessage[$getServerVar[botLogChannel];{newEmbed:{color:$getServerVar[color]}{title:Role given}{thumbnail:$if[$serverIcon==] https://cdn.lynnux.xyz/images/No-Server_Icon-found.png $else $serverIcon $endif}{footer:Given by @$userTag}{description:The <@&$findRole[$message[2]]> has been given to <@$findUser[$message[1]]>.
-Reason: \`\`\`
-No reason given.
-\`\`\`}}]
-                                    $endif
-                                    $title[Role given]
-                                    $description[The <@&$findRole[$message[2]]> has been given to <@$findUser[$message[1]]>.
-Reason: \`\`\`
-No reason given.
-\`\`\`]
-                                    $if[$serverIcon==]
-                                        $thumbnail[https://cdn.lynnux.xyz/images/No-Server_Icon-found.png]
-                                    $else
-                                        $thumbnail[$serverIcon]
+                                        $if[$checkContains[$toLowerCase[$message[1]];settings;setting]==true]
+                                            $description[**Command:** \`Settings\`
+    **aliases:** [\`setting\`\] 
+    **description:**  \`See your settings.\`
+    **Usage:** \`akira settings\`
+    
+    **Example:**
+    \`\`\`
+    akira settings
+    \`\`\`
+    **Note:** 
+    \`\`\`
+    to see the server setting you need "Admin" permission in discord or the "server-settings" permission in akira {akira perms add server-settings} 
+    \`\`\`
+    
+    **Module\:** \`Utility\`
+    ]
+                                        $else
+                                            $if[$checkContains[$toLowerCase[$message[1]];prefix;setting]==true]
+                                                $description[**Command:** \`prefix\`
+    **aliases:** [\`setprefix\`,\`set-prefix\`\] 
+    **description:**  \`Change your own or the server prefix.\`
+    **Usage:** \`akira prefix [newPrefix\] <user/server>\`
+    
+    **Example:**
+    \`\`\`
+    akira prefix ! user
+    akira prefix ?
+    \`\`\`
+    
+    **Module\:** \`Utility\`
+    ]
+                                            $else
+                                                $if[$checkContains[$toLowerCase[$message[1]];ban;ban]==true]
+                                                    $description[**Command:** \`prefix\`
+**aliases:** [\`setprefix\`,\`set-prefix\`\] 
+**description:**  \`Change your own or the server prefix.\`
+**Usage:** \`akira prefix [newPrefix\] <user/server>\`
+
+**Example:**
+\`\`\`
+akira prefix ! user
+akira prefix ?
+\`\`\`
+
+**Module\:** \`Utility\`
+]
+                                                $else
+                                                    $description[I couldn't found "$message" in my command list.]
+                                                $endif
+                                            $endif
+                                        $endif
                                     $endif
                                 $endif
-                            $else
-                                $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Can't kick this user!}{description:This user has a higher or the same role as me,
-if you want me to kick, put my highest role above the user's higest role.}{image:https://cdn.lynnux.xyz/images/RolePossition-giverole.png}}]                                
                             $endif
-                        $else
-                            $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Wrong usage!}{description:Wrong usage of the command \`giverole\`,
-Try to use
-\`\`\`
-a.giverole @$userTag[$findUser[$message[1]]] <role> {reason#LEFT_BRACKET#
-\`\`\`}}]   
                         $endif
-                    $else
-                        $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Wrong usage!}{description:Wrong usage of the command \`giverole\`,
-Try to use
-\`\`\`
-a.giverole <user> <role> {reason#LEFT_BRACKET#
-\`\`\`}}]                    
                     $endif
-                $else
-                    $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ No permissions!}{description:You do not have the right permission to use this command}{footer:Required permission = ManageRoles / Admin}}]
-                $endif
-            $else
-                $if[$checkContains[$userPerms;administrator]==true]
-                    $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Channel Ignored!}{description:An admin of this server choose to ignore this channel.
-You can stop ignoring this channel by using:
-\`\`\`
-a.listen
-\`\`\`}}]
-                $else
-                    $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Channel Ignored!}{description:An admin of this server choose to ignore this channel.}}]
                 $endif
             $endif
-        $else
-            $if[$checkContains[$userPerms;administrator]==true]
-                $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Module Disabled!}{description:An admin of this server chose to disable the \`giverole\` command.
-You can re-enable it by using:
-\`\`\`
-a.enable giverole
-\`\`\`}}]
-            $else
-                $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Module Disabled!}{description:An admin of this server chose to disable the \`giverole\` command.}}]
-            $endif
         $endif
-    $else
-        $if[$checkContains[$userPerms;administrator]==true]
-            $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Module Disabled!}{description:An admin of this server chose to disable the \`Moderation\` module.
-You can re-enable it by using:
-\`\`\`
-a.enable moderation
-\`\`\`}}]
-        $else
-            $channelSendMessage[$channelID;{newEmbed:{color:ff3333}{title:❌ Module Disabled!}{description:An admin of this server chose to disable the \`Moderation\` module.}}]
-        $endif
-    $endif
-
-    $textSplit[$findUser[$message[1]]-=OwO=-$authorID;-=OwO=-]
-
-    $onlyIf[$guildID!=;{newEmbed:{color:ff3333}{title:❌ Something went wrong!}{description:Please use this command in a server.}}]
-`})
-
-
-/*
-Text Split Map
-
-1 = User Found In Message 1
-2 = AuthorID
-*/
+        $addField[Extra links and information:;[[Invite akira\](https://discord.com/api/oauth2/authorize?client_id=738057910923296839&permissions=8&scope=bot%20applications.commands)\] | [Support Server\] | [Vote\] | [Website\] | [Premium\]]
+    
+    $textSplit[$message[1]1274,^-=-;1274,^-=-]
+    `})
+    /* 
+        name: "Userinfo",
+        aliases: ["User","user-info","whois","who-is"],
+        description: "show info about a user",
+    
+    $replaceText[$if[$replaceText[$if[$checkContains[$toLowerCase[owo];about;ping]==true];true;about]==true];false;$checkContains[$toLowerCase[owo];userinfo;whois;who-is;user-info;user];true;userinfo]
+    */
+    
