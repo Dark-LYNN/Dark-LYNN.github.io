e7157c09dd5073bb349939054a14cb44dca7b9d9
Dark-Lynn <65414822+Dark-LYNN@users.noreply.github.com>
2023-07-24 15:27:52 +0200

    de

--- sites/lynnux/assets/js/script.js
+++ sites/lynnux/assets/js/script.js
@@ -114,23 +114,23 @@ for (let i = 0; i < filterBtn.length; i++) {
 }
 
 // contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
+const form = document.getElementById("contact-form");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
+// add event to the form element
+form.addEventListener("submit", function (event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
 
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
+  // check form validation
+  if (form.checkValidity()) {
+    formBtn.removeAttribute("disabled");
+    // Submit the form programmatically
+    form.submit();
+  } else {
+    formBtn.setAttribute("disabled", "");
+  }
+});
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
