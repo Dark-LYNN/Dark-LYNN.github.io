703bc3a4c7087626922eb152ddf9aea6d6dcc3fb
Dark-Lynn <65414822+Dark-LYNN@users.noreply.github.com>
2023-07-27 20:52:27 +0200

    de

--- sites/lynnux/assets/js/script.js
+++ sites/lynnux/assets/js/script.js
@@ -158,12 +158,6 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-// Add event to the form submit event
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (checkFormValidity()) {
-    window.alert("Your message has been sent successfully!");
-  } else {
-    console.log("Please fill out all the required fields.");
-  }
-});
+if (window.location.href.includes("/?messageSuccess")) {
+  alert("Your message has been sent successfully!");
+}
