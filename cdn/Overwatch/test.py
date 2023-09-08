c2948a6ee79b72fd460673a54f6bd03885ec09d0
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-09-08 20:52:19 +0200

    de

--- cdn/Overwatch/test.py
+++ cdn/Overwatch/test.py
@@ -9,7 +9,7 @@ def generate_directory_listing(directory):
     listing = []
     for item in os.listdir(directory):
         item_path = os.path.join(directory, item)
-        item_type = "Directory" if os.path.isdir(item_path) else "File"
+        item_type = "<DIRECTORY>" if os.path.isdir(item_path) else os.path.splitext(item)[1].upper() + " file"
         item_size = "N/A" if os.path.isdir(item_path) else f"{os.path.getsize(item_path) / 1024:.2f} KB"
         item_date_modified = os.path.getmtime(item_path)
         relative_path = os.path.relpath(item_path, root_directory)  # Get relative path
