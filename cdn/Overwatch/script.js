89b55a6cd80716ef031f3e277ffa5aadce13a52a
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-09-08 19:10:23 +0200

    Update script.js

--- cdn/Overwatch/script.js
+++ cdn/Overwatch/script.js
@@ -1,28 +1,34 @@
 // script.js
 function displayDirectoryListing() {
-    fetch('./output.json') // 
+    fetch('output.json') // Replace with the path to your JSON file
         .then(response => response.json())
         .then(data => {
             const currentDirectory = window.location.pathname; // Get the current directory path from the URL
             const fileList = document.getElementById('fileList');
-            
+
             data.filter(item => {
-                // Check if the item belongs to the current directory and is not an HTML or JSON file
-                return item.name !== '..' && 
-                       item.name.startsWith(currentDirectory) &&
-                       !item.name.endsWith('.html') && 
-                       !item.name.endsWith('.json');
+                // Check if the item belongs to the current directory (based on the current path)
+                // and is not an HTML or JSON file
+                const itemName = item.name.split('/').pop(); // Extract the filename without path
+                return item.name.startsWith(currentDirectory) &&
+                       !itemName.endsWith('.html') &&
+                       !itemName.endsWith('.json');
             }).forEach(item => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${item.name}</td>
-                    <td>${item.type}</td>
-                    <td>${item.size}</td>
-                    <td>${new Date(item.dateModified * 1000).toLocaleString()}</td>
-                `;
-                fileList.appendChild(row);
+                // Extract the filename without path
+                const itemName = item.name.split('/').pop();
+
+                // Check if the item is not in a subdirectory
+                if (!itemName.includes('/')) {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${itemName}</td>
+                        <td>${item.type}</td>
+                        <td>${item.size}</td>
+                        <td>${new Date(item.dateModified * 1000).toLocaleString()}</td>
+                    `;
+                    fileList.appendChild(row);
+                }
             });
-            console.log('Current Directory:', currentDirectory);
         })
         .catch(error => console.error(error));
 }
