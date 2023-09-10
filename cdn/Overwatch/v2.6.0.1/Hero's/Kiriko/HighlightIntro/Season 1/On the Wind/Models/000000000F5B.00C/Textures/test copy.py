06d8d7df29b0b28b77fe7ae7e2f2a7ff0429feec
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-09-10 12:49:54 +0200

    de

---
+++ cdn/Overwatch/v2.6.0.1/Hero's/Kiriko/HighlightIntro/Season 1/On the Wind/Models/000000000F5B.00C/Textures/test copy.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import os
+import json
+
+# TIFF to PNG conversion and file deletion
+for file in os.listdir('.'):
+    if file.endswith('.tif'):
+        tif_path = file
+        png_path = os.path.splitext(file)[0] + '.png'
+
+        try:
+            im = Image.open(tif_path)
+            im.save(png_path)
+            os.remove(tif_path)  # Delete the original .tif file
+            print(f"Converted {tif_path} to {png_path} and removed original")
+        except Exception as e:
+            print(f"Failed to convert {tif_path}: {str(e)}")
+
+# JSON file generation
+current_directory = os.path.dirname(os.path.abspath(__file__))
+root_directory = os.path.join(current_directory)
+
+def generate_directory_listing(directory):
+    listing = []
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isfile(item_path) or os.path.isdir(item_path):
+            item_type = "&lt;DIRECTORY&gt;" if os.path.isdir(item_path) else os.path.splitext(item)[1].upper() + " file"
+            item_size = "N/A" if os.path.isdir(item_path) else f"{os.path.getsize(item_path) / 1024:.2f} KB"
+            item_date_modified = os.path.getmtime(item_path)
+            relative_path = os.path.relpath(item_path, root_directory)
+            listing.append({
+                "name": relative_path.replace("\\", "/"),
+                "type": item_type,
+                "size": item_size,
+                "dateModified": item_date_modified
+            })
+    return listing
+
+directory_data = generate_directory_listing(root_directory)
+
+output_json_file = os.path.join(root_directory, 'output.json')
+
+with open(output_json_file, 'w', encoding='utf-8') as json_file:
+    json.dump(directory_data, json_file, indent=4)
+
+print(f'Directory listing data has been generated and saved to {output_json_file}')
