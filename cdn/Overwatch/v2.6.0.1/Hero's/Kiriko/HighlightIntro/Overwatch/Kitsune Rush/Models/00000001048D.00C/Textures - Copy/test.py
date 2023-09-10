851db4ae77fb710ba3b67a497287a0dab4965d5c
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-09-10 12:35:49 +0200

    de

--- cdn/Overwatch/v2.6.0.1/Hero's/Kiriko/HighlightIntro/Overwatch/Kitsune Rush/Models/00000001048D.00C/Textures - Copy/test.py
+++ cdn/Overwatch/v2.6.0.1/Hero's/Kiriko/HighlightIntro/Overwatch/Kitsune Rush/Models/00000001048D.00C/Textures - Copy/test.py
@@ -1,8 +1,15 @@
+from PIL import Image
 import os
 import json
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# TIFF to PNG conversion
+for file in os.listdir('.'):
+    if file.endswith('.tif'):
+        im = Image.open(file)
+        im.save(os.path.splitext(file)[0] + '.png')
 
+# JSON file generation
+current_directory = os.path.dirname(os.path.abspath(__file__))
 root_directory = os.path.join(current_directory)
 
 def generate_directory_listing(directory):
@@ -13,9 +20,9 @@ def generate_directory_listing(directory):
             item_type = "&lt;DIRECTORY&gt;" if os.path.isdir(item_path) else os.path.splitext(item)[1].upper() + " file"
             item_size = "N/A" if os.path.isdir(item_path) else f"{os.path.getsize(item_path) / 1024:.2f} KB"
             item_date_modified = os.path.getmtime(item_path)
-            relative_path = os.path.relpath(item_path, root_directory)  
+            relative_path = os.path.relpath(item_path, root_directory)
             listing.append({
-                "name": relative_path.replace("\\", "/"),  
+                "name": relative_path.replace("\\", "/"),
                 "type": item_type,
                 "size": item_size,
                 "dateModified": item_date_modified
