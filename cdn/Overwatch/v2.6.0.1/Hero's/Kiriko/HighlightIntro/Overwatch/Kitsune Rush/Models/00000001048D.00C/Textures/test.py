f605fc616ae5c59babea6d6f13c9f93a4f306158
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-09-10 12:38:47 +0200

    de

--- cdn/Overwatch/v2.6.0.1/Hero's/Kiriko/HighlightIntro/Overwatch/Kitsune Rush/Models/00000001048D.00C/Textures/test.py
+++ cdn/Overwatch/v2.6.0.1/Hero's/Kiriko/HighlightIntro/Overwatch/Kitsune Rush/Models/00000001048D.00C/Textures/test.py
@@ -1,13 +1,25 @@
+from PIL import Image
 import os
 import json
 
-# Get the current directory of the file
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# TIFF to PNG conversion and file deletion
+for file in os.listdir('.'):
+    if file.endswith('.tif'):
+        tif_path = file
+        png_path = os.path.splitext(file)[0] + '.png'
 
-# Append the rest of the path to it
+        try:
+            im = Image.open(tif_path)
+            im.save(png_path)
+            os.remove(tif_path)  # Delete the original .tif file
+            print(f"Converted {tif_path} to {png_path} and removed original")
+        except Exception as e:
+            print(f"Failed to convert {tif_path}: {str(e)}")
+
+# JSON file generation
+current_directory = os.path.dirname(os.path.abspath(__file__))
 root_directory = os.path.join(current_directory)
 
-# Function to generate directory listing data for a given directory
 def generate_directory_listing(directory):
     listing = []
     for item in os.listdir(directory):
@@ -16,22 +28,19 @@ def generate_directory_listing(directory):
             item_type = "&lt;DIRECTORY&gt;" if os.path.isdir(item_path) else os.path.splitext(item)[1].upper() + " file"
             item_size = "N/A" if os.path.isdir(item_path) else f"{os.path.getsize(item_path) / 1024:.2f} KB"
             item_date_modified = os.path.getmtime(item_path)
-            relative_path = os.path.relpath(item_path, root_directory)  # Get relative path
+            relative_path = os.path.relpath(item_path, root_directory)
             listing.append({
-                "name": relative_path.replace("\\", "/"),  # Use forward slashes in the path
+                "name": relative_path.replace("\\", "/"),
                 "type": item_type,
                 "size": item_size,
                 "dateModified": item_date_modified
             })
     return listing
 
-# Generate the directory listing data for the root directory
 directory_data = generate_directory_listing(root_directory)
 
-# Define the path for the output JSON file
 output_json_file = os.path.join(root_directory, 'output.json')
 
-# Write the directory listing data to the output JSON file
 with open(output_json_file, 'w', encoding='utf-8') as json_file:
     json.dump(directory_data, json_file, indent=4)
 
