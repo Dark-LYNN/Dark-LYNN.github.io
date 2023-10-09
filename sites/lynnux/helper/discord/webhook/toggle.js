48a2e8993a7105a2c2035bd07a098226fe363f7a
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-10-09 17:32:06 +0200

    de

--- sites/lynnux/helper/discord/webhook/toggle.js
+++
@@ -1,8 +0,0 @@
-function toggle()
-{
-    if (field_toggle.checked){
-        document.getElementById("embed").style.display = "inline";
-    }else{
-        document.getElementById("embed").style.display = "none";
-    }
-}
